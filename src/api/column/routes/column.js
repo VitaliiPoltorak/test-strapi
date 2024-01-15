ab22138880834c24e264bae2c10bce5b3a929c54
Vitaliy <vitaliypoltorak91@gmail.com>
2024-01-15 17:51:16 +0200

    tet

--- src/api/column/routes/column.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * column router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::column.column');
--- src/api/column/services/column.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * column service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::column.column');
