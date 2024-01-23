e1ea0e1cd85b5401515ec7a13a8a3a9725a9f526
Vitaliy <vitaliypoltorak91@gmail.com>
2024-01-23 15:48:47 +0200

    update order changing logic filtred by columnId

--- src/api/task/controllers/task.js
+++ src/api/task/controllers/task.js
@@ -10,11 +10,18 @@ module.exports = createCoreController('api::task.task', ({strapi}) => ({
   async updateOrder(ctx) {
     const {id} = ctx.params;
     const {body} = ctx.request;
-    const {order} = body.data;
-    const tasks = await strapi.entityService.findMany('api::task.task', {data: {id}});
+    const {order, columnId} = body.data;
+
+    if ( !order || !columnId ) {
+      return 'Order or columnId is missing'
+    }
+
+    const tasks = await strapi.entityService.findMany('api::task.task', {data: {columnId}});
+
     const updatedTaskOrder = tasks.find(task => task.id === +id).order
 
-    if (order !== undefined) {
+    if (order) {
+
       for (const task of tasks) {
         const taskId = task.id
         const taskOrder = task.order
@@ -29,6 +36,6 @@ module.exports = createCoreController('api::task.task', ({strapi}) => ({
       }
     }
 
-    return `done, order: ${order} , id: ${id}`
+    return tasks
   },
 }));
