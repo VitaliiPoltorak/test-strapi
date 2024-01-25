3faf3c05a0d6c5bf3bb8e24b2b86092fa28342fb
Vitaliy <vitaliypoltorak91@gmail.com>
2024-01-25 12:28:41 +0200

    dec order

--- src/api/task/controllers/task.js
+++ src/api/task/controllers/task.js
@@ -15,27 +15,23 @@ module.exports = createCoreController('api::task.task', ({strapi}) => ({
     if ( !order || !columnId ) {
       return 'Order or columnId is missing'
     }
-
     const tasks = await strapi.entityService.findMany('api::task.task', {data: {columnId}});
-
     const updatedTaskOrder = tasks.find(task => task.id === +id).order
-
     if (order) {
-
       for (const task of tasks) {
         const taskId = task.id
         const taskOrder = task.order
-
         if (taskId === +id) {
           await strapi.entityService.update('api::task.task', id, {data: {order:order}})
         }
         if (taskId !== +id && taskOrder >= order && taskOrder < updatedTaskOrder) {
-
           await strapi.entityService.update('api::task.task', `${taskId}`, {data: {order: taskOrder + 1}})
+        }
+        if (taskId !== +id && taskOrder <= order && taskOrder > updatedTaskOrder) {
+          await strapi.entityService.update('api::task.task', `${taskId}`, {data: {order: taskOrder - 1}})
         }
       }
     }
-
-    return tasks
+    return 'Orders updated'
   },
 }));
