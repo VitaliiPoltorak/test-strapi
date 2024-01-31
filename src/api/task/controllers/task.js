a3ca69772eebd69ce59b015562c387dae471da44
Vitaliy <vitaliypoltorak91@gmail.com>
2024-01-31 16:26:49 +0200

    upd order logic

--- src/api/task/controllers/task.js
+++ src/api/task/controllers/task.js
@@ -51,20 +51,29 @@ module.exports = createCoreController('api::task.task', ({strapi}) => ({
       return 'Order or columnId is missing'
     }
 
-    const tasks = await strapi.entityService.findMany('api::task.task', {data: {columnId}});
-    const updatedTaskOrder = tasks.find(task => task.id === +id).order
-
+    const tasks = await strapi.entityService.findMany('api::task.task', {
+      sort: 'order',
+      filters: {
+        $and: [
+          {
+            columnID: `${columnId}`,
+          },
+        ],
+      },
+    });
+    const prevTaskOrder = tasks.find(task => task.id === +id).order
     if (order) {
+
       for (const task of tasks) {
         const taskId = task.id
         const taskOrder = task.order
         if (taskId === +id) {
           await strapi.entityService.update('api::task.task', id, {data: {order: order}})
         }
-        if (taskId !== +id && taskOrder >= order && taskOrder < updatedTaskOrder) {
+        if (taskId !== +id && taskOrder >= order && taskOrder < prevTaskOrder) {
           await strapi.entityService.update('api::task.task', `${taskId}`, {data: {order: taskOrder + 1}})
         }
-        if (taskId !== +id && taskOrder <= order && taskOrder > updatedTaskOrder) {
+        if (taskId !== +id && taskOrder <= order && taskOrder > prevTaskOrder) {
           await strapi.entityService.update('api::task.task', `${taskId}`, {data: {order: taskOrder - 1}})
         }
       }
@@ -88,7 +97,6 @@ module.exports = createCoreController('api::task.task', ({strapi}) => ({
         await strapi.entityService.update('api::task.task', `${prevTasks[i - 1].id}`, {data: {order: i}})
       }
     }
-
 
     return 'Orders updated'
   },
