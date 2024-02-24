4c5aff8cbaf31fd72e2f5a4a9323906ea9e4230c
Vitaliy <vitaliypoltorak91@gmail.com>
2024-02-24 19:47:52 +0200

    fing

--- src/api/fingerprint/controllers/fingerprint.js
+++ src/api/fingerprint/controllers/fingerprint.js
@@ -4,6 +4,47 @@
  * fingerprint controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const {createCoreController} = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::fingerprint.fingerprint');
+module.exports = createCoreController('api::fingerprint.fingerprint', ({strapi}) => ({
+  async create(ctx) {
+    const {body} = ctx.request;
+    const {data} = body;
+
+    if (!data) {
+      return 'Data is missing';
+    }
+
+    return await Promise.all(
+      async function () {
+        await strapi.entityService.create('api::fingerprint.fingerprint', {
+          data,
+        })
+      },
+      async function () {
+        await strapi.entityService.create('api::column.column', {
+          data,
+        })
+      },
+    );
+  },
+}));
+
+
+const startColumnData = [
+  {
+    title: 'To Do',
+    boardId: 1,
+    custom: false,
+  },
+  {
+    title: 'In Progress',
+    boardId: 1,
+    custom: false,
+  },
+  {
+    title: 'Done',
+    boardId: 1,
+    custom: false,
+  },
+]
